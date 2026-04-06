92c1a31702c7701b7691f336d2bdbdf46fdc0252
marodar2 <marodar2@asu.edu>
2026-04-06 03:23:31 -0700

    Update pitchover.m

--- pitchover.m
+++ pitchover.m
@@ -1,4 +1,4 @@
-function bit = pitchover(t, S, simparams)
+function scale = pitchover(t, S, simparams)
 
 s = S(1);
 h = S(2);
@@ -6,13 +6,9 @@ gamma = S(3);
 v = S(4);
 m = S(5);
 
-if h < simparams.pitchover_start || v < 300
-    bit = 0;
-    return;
-end
-
+% Current (failed) pitchover implementation
 gamma_target = deg2rad(5);
 gamma_band = deg2rad(60);
 
-bit = (gamma - gamma_target) / gamma_band;
-bit = max(0, min(1, bit));
+scale = (gamma - gamma_target) / gamma_band;
+scale = max(0, min(1, scale));
